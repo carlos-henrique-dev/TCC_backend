8f2789f2e569fefaeacc1172c0610468ba3304de
carlos henrique <carloshenrique@carloss-MacBook-Pro.local>
2019-10-27 06:32:24 -0400

    Altera a configuração de url do BD

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,19 +1,13 @@
 /* configuração do acesso ao mongo e outras configs relacionadas ao BD */
 
 const mongoose = require('mongoose');
-const config = require('./index');
 
 module.exports = function (app) {
   // Conecta no MongoDB
-  mongoose.connect(/* config.mongoUrl */ 'mongodb://localhost:27017/TCCdatabase', {
+  mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
   });
-  /* mongoose.Promise = global.Promise;
-
-  process.on("SIGINT", cleanup);
-  process.on("SIGTERM", cleanup);
-  process.on("SIGHUP", cleanup); */
 
   if (app) {
     app.set('mongoose', mongoose);
